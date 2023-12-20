5a27bdc7ee9a6cca7845fe7e4fb4aa2460efd184
David Halek <david.halek@dynamo.works>
2023-12-20 09:08:33 -0600

    all sheets will run now

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openpyxl as xl
 import os
 import copy
-import numpy as np
+# import numpy as np
 
 # Forests where all districts are inactive as well - double check these later
 INACTIVE_FORESTS = [ 110108, 110105, 110112, 110405, 110608, 110611, 110620, ]
@@ -18,48 +18,49 @@ CLEAR_SHEETS = [ VERIFIED, CHECK, UPDATE ]
 
 def check_worksheet(pals, wb, results):
     db = dict()
-    sheet = DB_SHEETS[0]
-    ws = wb[sheet]
-    db_sheet = results[sheet]
-    db_sheet.delete_rows(1, 5000)
-    db_sheet.append(["Unit Number", "Problem", "Current Name", "CORRECT Name", "isActive", "CORRECT isActive"])
-    db_remove_me = [ ]
-    for rownum in range(2, ws.max_row+1):
-        u = ws.cell(rownum, 1).value
-        n = ws.cell(rownum, 2).value
-        a = True if ws.cell(rownum, 3).value == 1 else False
-        db[u] = [n,a]
-    unit_missing = pals.keys() - db.keys()
-    for unit in db:
-        if unit in pals.keys():
-            pals_name = pals[unit][0]
-            pals_active = pals[unit][1]
-            db_name = db[unit][0]
-            db_active = db[unit][1] == 1
-            if pals_name == db_name and pals_active == db_active:
-                results[VERIFIED].append([unit, pals_name, pals_active])
+    for sheet in DB_SHEETS:
+    # sheet = DB_SHEETS[0]
+        ws = wb[sheet]
+        db_sheet = results[sheet]
+        db_sheet.delete_rows(1, 5000)
+        db_sheet.append(["Unit Number", "Problem", "Current Name", "CORRECT Name", "isActive", "CORRECT isActive"])
+        db_remove_me = [ ]
+        for rownum in range(2, ws.max_row+1):
+            u = ws.cell(rownum, 1).value
+            n = ws.cell(rownum, 2).value
+            a = True if ws.cell(rownum, 3).value == 1 else False
+            db[u] = [n,a]
+        unit_missing = pals.keys() - db.keys()
+        for unit in db:
+            if unit in pals.keys():
+                pals_name = pals[unit][0]
+                pals_active = pals[unit][1]
+                db_name = db[unit][0]
+                db_active = db[unit][1] == 1
+                if pals_name == db_name and pals_active == db_active:
+                    results[VERIFIED].append([unit, pals_name, pals_active])
+                else:
+                    info = [''] * 6
+                    info[0] = unit
+                    if pals_name != db_name:
+                        info[1] = 'Name '
+                        info[2] = db_name
+                        info[3] = pals_name
+                    if pals_active != db_active:
+                        info[1] += 'Active'
+                        info[4] = db_active
+                        info[5] = pals_active
+                    results[ws.title].append(info)
+                #
+                # del db[unit]
             else:
-                info = [''] * 6
-                info[0] = unit
-                if pals_name != db_name:
-                    info[1] = 'Name '
-                    info[2] = db_name
-                    info[3] = pals_name
-                if pals_active != db_active:
-                    info[1] += 'Active'
-                    info[4] = db_active
-                    info[5] = pals_active
-                results[ws.title].append(info)
-            #
-            # del db[unit]
-        else:
-            db_remove_me.append([unit, "Not in PALS", db[unit][0], db[unit][1]])
-            # del db[unit]
-            # print(f"================================> { unit }  *db[unit] ")
-    for line in db_remove_me:
-        results[ws.title].append(line)
-    for unit in unit_missing:
-        results[ws.title].append([unit, "In PALS, not here", pals[unit][0], pals[unit][1]])
+                db_remove_me.append([unit, "Not in PALS", db[unit][0], db[unit][1]])
+                # del db[unit]
+                # print(f"================================> { unit }  *db[unit] ")
+        for line in db_remove_me:
+            results[ws.title].append(line)
+        for unit in unit_missing:
+            results[ws.title].append([unit, "In PALS, not here", pals[unit][0], pals[unit][1]])
     results.save(RESULTS_XL)
 
 # Generate a dict of ret[unit_num] = (name, active)

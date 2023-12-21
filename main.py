b26d89b67db352659c38d0e047f8ebe64101f8a4
David Halek <david.halek@dynamo.works>
2023-12-21 08:24:02 -0600

    all sheets called, no duplicates i think

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import copy
 # Forests where all districts are inactive as well - double check these later
 INACTIVE_FORESTS = [ 110108, 110105, 110112, 110405, 110608, 110611, 110620, ]
 RESULTS_XL = 'results.xlsx'
-INPUT_XL = r'Units Comparison as of 2023-11-09 3_58 PM ET (1).xlsx'
+INPUT_XL = r'Units Comparison as of 2023-11-09 3_58 PM ET.xlsx'
 PALS = "PALS Prod"
 DB_SHEETS = [ 'CARA Test', 'CARA Prod2', 'DataMart Test', 'DataMart Prod', ]
 NOPE = 'NOPE'
@@ -17,16 +17,17 @@ UPDATE = 'Update These'
 CLEAR_SHEETS = [ VERIFIED, CHECK, UPDATE ]
 
 def check_worksheet(pals, wb, results):
-    db = dict()
     for sheet in DB_SHEETS:
     # sheet = DB_SHEETS[0]
         ws = wb[sheet]
+        db = dict()
         db_sheet = results[sheet]
+        print(db_sheet.title)
         db_sheet.delete_rows(1, 5000)
         db_sheet.append(["Unit Number", "Problem", "Current Name", "CORRECT Name", "isActive", "CORRECT isActive"])
         db_remove_me = [ ]
         for rownum in range(2, ws.max_row+1):
-            u = ws.cell(rownum, 1).value
+            u = f'{ws.cell(rownum, 1).value}'
             n = ws.cell(rownum, 2).value
             a = True if ws.cell(rownum, 3).value == 1 else False
             db[u] = [n,a]
@@ -38,7 +39,7 @@ def check_worksheet(pals, wb, results):
                 db_name = db[unit][0]
                 db_active = db[unit][1] == 1
                 if pals_name == db_name and pals_active == db_active:
-                    results[VERIFIED].append([unit, pals_name, pals_active])
+                    results[VERIFIED].append([unit, pals_name, pals_active, db_sheet.title])
                 else:
                     info = [''] * 6
                     info[0] = unit
@@ -50,18 +51,14 @@ def check_worksheet(pals, wb, results):
                         info[1] += 'Active'
                         info[4] = db_active
                         info[5] = pals_active
-                    results[ws.title].append(info)
-                #
-                # del db[unit]
-            else:
+                    db_sheet.append(info)
+            elif db[unit][1]: #todo inactive ones should be removed, or no? hiding them for now
                 db_remove_me.append([unit, "Not in PALS", db[unit][0], db[unit][1]])
-                # del db[unit]
-                # print(f"================================> { unit }  *db[unit] ")
         for line in db_remove_me:
             results[ws.title].append(line)
         for unit in unit_missing:
             results[ws.title].append([unit, "In PALS, not here", pals[unit][0], pals[unit][1]])
-    results.save(RESULTS_XL)
+        results.save(RESULTS_XL)
 
 # Generate a dict of ret[unit_num] = (name, active)
 def pals_setup(ws):
